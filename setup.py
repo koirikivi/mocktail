0f699d551ff3b628c386d97c01d5e3ddbfacf597
Rainer Koirikivi <rainer@koirikivi.fi>
2020-04-16 01:40:19 +0300

    Prepare for PyPi release

--- setup.py
+++ setup.py
@@ -15,5 +15,16 @@ setup(
             'pytest-watch',
             'flake8',
         ]
-    }
+    },
+    keywords=['testing', 'mock', 'unittest', 'mockito', 'when'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
