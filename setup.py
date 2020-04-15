ddaa506c6e9d0b8a0a20501deeb8be1fa440dd67
Rainer Koirikivi <rainer@koirikivi.fi>
2020-04-16 01:47:59 +0300

    Small alterations for nicer PyPi page

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+from os import path
 from setuptools import setup, find_packages
+
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='mocktail',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/koirikivi/mocktail.git',
     author='Rainer Koirikivi',
     author_email='rainer@koirikivi.fi',
     description='Non-alcoholic Mockito-style when-then utilities for unittest.mock',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[],
     extras_require={
@@ -20,7 +29,8 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Testing :: Mocking',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
